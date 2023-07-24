9ab81819a19c1903f6692c6719a995e10cb428e8
sebelousov <bsn@list.ru>
2023-07-24 16:21:34 +0300

    add connect to db in index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const path = require('path')
+const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const PORT = process.env.PORT || 3000
+const URL = 'mongodb://localhost:27017/Todos'
 
 const homeRoutes = require('./routes/home')
 const aboutRoutes = require('./routes/about')
@@ -31,86 +33,16 @@ app.use('/add', addRoutes)
 app.use('/cart', cartRoutes)
 app.use('/todos', todosRoutes)
 
-app.listen(PORT, () => {
-    console.log(`App listening to port ${PORT}`)
-})
+start()
 
-// const express = require('express')
-// const { engine } = require('express-handlebars')
-// const app = express()
-// const PORT = process.env.PORT || 3000
+async function start() {
+    try {
+        await mongoose.connect(URL)
+        app.listen(PORT, () => {
+            console.log(`App listening to port ${PORT}`)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
 
-// app.engine('handlebars', engine());
-// app.set('view engine', 'handlebars');
-// app.set('views', './views');
-
-// app.get('/', (req, res) => {
-//     // res.send('Hello, World!')
-//     res.render('home');
-// })
-
-// app.listen(PORT, () => {
-//     console.log(`App listening to port ${PORT}`)
-// })
-
-// const express = require('express')
-
-// const app = express()
-// const PORT = process.env.PORT || 3000
-
-// const handlebars = require('express-handlebars')
-
-// app.set('view engine', 'handlebars')
-
-// app.engine('handlebars', handlebars({
-//     layoutsDir: __dirname + '/views/layouts'
-// }))
-
-// app.use(express.static('public'))
-// app.get('/', (req, res) => {
-//     res.render('main', {
-//         layout: 'index'
-//     })
-// })
-
-// app.listen(PORT, () => {
-//     console.log(`App listening to port ${PORT}`)
-// })
-
-// const path = require('path')
-// const express = require('express')
-// const exphbs = require('express-handlebars')
-// const PORT = process.env.PORT || 3000
-
-// const app = express()
-
-// const hbs = exphbs.create({
-//     defaultLayout: 'main',
-// 	extname: 'hbs'
-// })
-
-// app.engine('hbs', hbs.engine)
-// app.set('view engine', 'hbs')
-// app.set('views', 'views')
-
-
-
-// Use static html-pages
-// app.get('/', (req, res) => {
-//     res.status(200)
-//     res.sendFile(path.join(__dirname, 'views', 'index.html'))
-// })
-
-// app.get('/about', (req, res) => {
-//     res.status(200)
-//     res.sendFile(path.join(__dirname, 'views', 'about.html'))
-// })
-
-// app.get('/404', (req, res) => {
-//     res.status(200)
-//     res.sendFile(path.join(__dirname, 'views', '404.html'))
-// })
-
-// app.listen(PORT, () => {
-//     console.log(`Server is running from port ${PORT}.`)
-// })
