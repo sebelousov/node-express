234b50c49511b8d3a2a757e745dc4176804a23de
sebelousov <bsn@list.ru>
2023-07-24 17:01:11 +0300

    add controller directory, add todo-controller.js, cart-controller.js

---
+++ controller/cart-controller.js
@@ -0,0 +1,22 @@
+const getAllTodos = () => {
+
+}
+
+const saveTodo = (todo) => {
+    
+}
+
+const updateTodoById = (id) => {
+
+}
+
+const deleteTodoById = (id) => {
+    
+}
+
+module.exports = {
+    getAllTodos,
+    saveTodo,
+    updateTodoById,
+    deleteTodoById
+}
---
+++ controller/todo-controller.js
@@ -0,0 +1,27 @@
+const getAllTodos = () => {
+
+}
+
+const getTodoById = (id) => {
+
+}
+
+const saveTodo = (todo) => {
+    
+}
+
+const updateTodoById = (id) => {
+
+}
+
+const deleteTodoById = (id) => {
+    
+}
+
+module.exports = {
+    getAllTodos,
+    getTodoById,
+    saveTodo,
+    updateTodoById,
+    deleteTodoById
+}
